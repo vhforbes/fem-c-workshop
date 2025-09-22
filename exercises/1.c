f4096621c3a8db4cacd61849fbbc4821639925e3
vhforbes <vhforbes@gmail.com>
2025-09-22 08:02:43 -0300

    2c exercise

--- exercises/1.c
+++ exercises/1.c
@@ -1,4 +1,4 @@
-// #include <stdio.h>
+#include <stdio.h>
 #include <unistd.h>
 
 // 👉 First, build and run the program.
@@ -12,13 +12,14 @@ int main() {
     //    changing the length to 14.
     // 👉 Try making the length less than 14. What happens when you run the program?
     // 👉 Try making the length much longer than 14, like 200. What happens? 😱
-    write(1, "Hello, World!", 13);
+    write(1, "Hello, World!\n", 13);
 
     // 👉 Try uncommenting the next 2 lines, as well as the #include <stdio.h> at the top.
     //    (You'll want to change the length of write() above back to 14 first!)
     // 👉 Try adding a second number, named num2. Set it to something other than 42.
-    // int num = 42;
-    // printf("The number is: %d\n", num);
+    int num = 42;
+    int num1 = 22;
+    printf("The number is: %d\n %d\n", num, num1);
 
     // 👉 Try having printf print *both* numbers.
     //    Hint: you'll need to make 2 changes to printf's arguments to do this!
--- exercises/2.c
+++ exercises/2.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 
 // 👉 First, build and run the program.
 //
@@ -11,14 +12,14 @@ int main() {
     char *header = "HTTP/1.1 200 OK";
 
     // 👉 Try replacing this `15` with a call to `strlen` (and `#include <string.h>` above!)
-    write(1, header, 15);
+    write(1, header, strlen(header));
 
     // 👉 After you're using `strlen` above, try adding a `\0` (with the backslash)
     // inside the definition of `header`, e.g.  "HTT\0P/1.1 200 OK" - this inserts
     // a zero byte in the string. Before you run the program, what do you think it will print?
 
     // 👉 Try changing this `%s` to `%zu` (ignore the compiler warning).
-    printf("\n\nThat output was from write(). This is from printf: %s\n", header);
+    printf("\n\nThat output was from write(). This is from printf: %zu\n", header);
 
     // 👉 Try changing the `%zud` back to `%s` and then replace the printf call's
     // last argument (originally `header`) with this argument instead: `(char*)123456`
